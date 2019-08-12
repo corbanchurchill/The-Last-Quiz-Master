2fecd99840c8dc46ee238dc1931b006ded995fd1
corbanchurchill <53544982+corbanchurchill@users.noreply.github.com>
2019-08-12 16:12:57 +1200

    0.1.3
    Started the Profession selection menu

--- main.py
+++ main.py
@@ -30,16 +30,7 @@ def clear():
   else:
     system('clear')
 #||-------------------||#
-
-
-#||===================||#
-#||===MENU=CONTEXT====||#
-#||===================||#
-def about_text():
-  print("The Last Quiz Master is a text based quiz rpg game written\nin python. It is based off the story of an unlucky adventurer,\nwho is lost in the land of Uulgurm'ath.This ancient land is\nriddled with many hostile creatures. To survive you must have\nwitt, intelligence and bravery. You decide how you go about in\nthis adventure.")
-  time.sleep(9)
-  print("\nTo fulfil your burning curiosity, here is my github link: \nhttps://github.com/corbanchurchill/The-Last-Quiz-Master\n\naGVyZSBpcyB0aGUgc291cmNlIG9mIG1vc3Qgb2YgbXkgcXVlc3Rpb25zOg0KaHR0cHM6Ly9lbi53aWtpcGVkaWEub3JnL3dpa2kvTGlzdF9vZl9jb21tb25fbWlzY29uY2VwdGlvbnM=")
-
+#||====Health=========||3
 
 
 #||===================||#
@@ -182,8 +173,7 @@ def ent_old_hag():
            `'-----'`      \        __.'
                            `-..--'`
       ==============================================
-       |=-=-=||STATS:    ||INVENTORY:             |               
-       |WITCH||DMG:    25||fermented frog legs    |
+       |=-=-=||STATS:    ||INVENTORY:             |        |WITCH||DMG:    25||fermented frog legs    |
        |=-=-=||HEALTH: 15||ancient enchanted stick|
       ==============================================
 """
@@ -263,7 +253,7 @@ clear()
 loop = 0
 while loop == 0:
   clear()
-  menu = input(main_menu)
+  menu = input(main_menu).lower()
 #||=====ABOUT======||#
   if menu == '3':
     clear()
@@ -284,7 +274,7 @@ Input 'b' to return: """)
     clear()
     print(instruction_screen)
     time.sleep(0.5)
-    print("\n1. Please only input lowercase letters.\n\n")
+    print("\n1. Please only input lowercase letters.\nThis is due to the fact that\nim lazy and cant be bothered\nmaking it to react to every varient\nof whatever you need to input\n")
     menu = input("Input 'b' to return: ")
   if menu == 'b':
     continue
@@ -303,8 +293,24 @@ Input 'b' to return: """)
     menu = input("Input 'b' to return: ")
   if menu == 'b':
     continue
+  if menu == '05':
+    clear()
+    print(
+      """
+╔═╗═╗ ╦╔═╗╦  ╔═╗╦╔╦╗╔═╗
+║╣ ╔╩╦╝╠═╝║  ║ ║║ ║ ╚═╗
+╚═╝╩ ╚═╩  ╩═╝╚═╝╩ ╩ ╚═╝
+      """
+      )
+    time.sleep(0.5)
+    print("Here lie all of the knowen exploits in my terrible code!")
+    time.sleep(0.5)
+    print("1. In the main menu you can actually press any key to leave.")
+    menu = input("Press 'any key' to return:\n> ")
+  if menu == 'b':
+    continue
 #||------------------||#
-  
+
 clear()
 print(char_create_menu)
 time.sleep(0.5)
@@ -318,7 +324,89 @@ s_name = input("So {}, What is your Surname?\n> ".format(f_name))
 time.sleep(0.5)
 clear()
 print(char_create_menu)
-print("A pleasure to make your acquaintance, on this beautiful day! {} {}!".format(f_name, s_name))
+print("A pleasure to make your acquaintance, on this beautiful day!\n{} {}!".format(f_name, s_name))
+time.sleep(3)
 clear()
-print(char_create_menu)
+#Profession Selection loop.
+loop = 0
+while loop == 0:
+  clear()
+  print(char_create_menu)
+  time.sleep(0.25)
+  prof = input(
+    """
+What is your preferred profession?
+==================================
+1. Jack of All Trades
+2. Elemental Mage
+3. Reclusive Thief
+4. Standard Warrior
+>  """)
+#Jack of All Trades
+  if prof == '1':
+    clear()
+    print(char_create_menu)
+    time.sleep(0.25)
+    print(
+      """
+Brief Overview:
+=====================================================
+The Jack of All Trades is an interesting profession!
+This is due to the fact that you dont specialize in
+Just one thing! Although you are not exactly the best
+at anything either. It would be a spoiler for me to
+inform you on anything more! Although I can spare you
+one last secret...
+    """)
+    time.sleep(0.5)
+    print(
+      """
+Special ability:
+=======================================================
+During a battle, if your health drops below 2 points,
+you can call upon a your ancestors to summon a horrifying
+Wendigo to assist you in the battle.
+      """)
+    prof = input("To select input: '1' To return: 'b'\n> ")
+    if prof == '1':
+      break
+    else:
+      continue
+#Elemental Mage
+  if prof == '2':
+    clear()
+    print(char_create_menu)
+    time.sleep(0.25)
+    print(
+      """
+Brief Overview:
+=======================================================
+The Elemental Mage is the profession of Magic utilized
+by only the most skilled Mages of any type! While a 
+normal mage would only utilize and master one or two 
+elements the Elemental Mage is able master all of the 5
+elements and is also able to use them in a complemetary
+manor. Sadly the secrets of magic where long ago
+released so their special abilities are common knowlege.""")
+    time.sleep(0.5)
+    print(
+      """
+Special Ability:
+=======================================================
+As an Elemental Mage you are able to heal upto 50% of
+your remaining health, this even works if you are healthy
+but be warned the lower your health the less you are able
+heal.""")
+    prof = input("Input '1' to select, or Input 'b' to return:\n> ")
+  if prof == '1':
+    break
+  else:
+    continue
+    
+    
+
+
+
+
+
 
